4ceeac7141bbb8c02184f3b841bfaec044270c0b
Kev McCarthy <kev@totallyratted.com>
2019-02-25 08:15:21 +0000

    ECStraordinary ECSeption

--- AWSome.ECStraordinary/Config/ECSeption.cs
+++ AWSome.ECStraordinary/Config/ECSeption.cs
@@ -5,15 +5,15 @@ using System;
 namespace Liberator.AWSome.ECStraordinary.Config
 {
     /// <summary>
-    /// An exception
+    /// An ECStraordinary exception has been thrown
     /// </summary>
     public class ECSeption : Exception
     {
         /// <summary>
-        /// 
+        /// An ECStraordinary exception has been thrown
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="exception"></param>
+        /// <param name="message">The message for the ECSeption</param>
+        /// <param name="exception">The underlying AmazonECSException</param>
         public ECSeption(string message, AmazonECSException exception)
         {
             Console.WriteLine(message);
